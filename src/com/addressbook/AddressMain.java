6bc09a8a11bd086a205c763a23a79ab4ea1f7cb1
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-02 22:08:29 +0530

    Added use case 1 : Ability to create a new contact

--- src/com/addressbook/AddressMain.java
+++ src/com/addressbook/AddressMain.java
@@ -4,7 +4,8 @@ public class AddressMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System!");
-
+		AddressBook addressbook = new AddressBook("John","Doe","Elite","Bantwal","Karnataka","574219","9550012911","abcd@gmail.com");
+		addressbook.display();
 	}
 
 }
